4f908c1b939bbd1351d538938cacb4ccfdb03176
E-Eremiev <misterskriberr@gmail.com>
2025-04-29 23:40:07 +0300

    Add project files.

---
+++ OOP/Books.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Books
+    {
+
+        private string title;
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        private string author;
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+            set
+            {
+                author = value;
+            }
+        }
+        private int year;
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        } 
+        private bool isAvaliable;
+        public bool IsAvailable
+        {
+            get
+            {
+                return isAvaliable;
+            }
+            set
+            {
+               isAvaliable = value;
+            }
+        }
+        public Books(string title, string author, int year,bool isAvaliable)
+        {
+            Title = title;           
+            Author = author;           
+            Year = year;          
+            IsAvailable = isAvaliable;   
+        }
+
+       
+        public void BorrowBook()
+        {
+            IsAvailable = false;
+        }
+        public void ReturnBook()
+        {
+            IsAvailable = true;
+        }
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Въведете брой книги:");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+            Books[] books = BooksInformation(n);
+            Print(books);
+        }
+        static Books[] BooksInformation(sbyte n)
+        {
+            Books[] books = new Books[n];
+            Console.WriteLine("Въведи <заглавие><автор><година на издава><в наличност>(True/False)");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"{i+1}.Книга:");
+                string[] infoparts = Console.ReadLine().Split(' ');
+                string title = infoparts[0];
+                string author = infoparts[1];
+                short year = short.Parse(infoparts[2]);
+                bool isAvailiable = bool.Parse(infoparts[3]);
+                books[i] = new Books(title,author,year,isAvailiable);
+            }
+
+            return books;
+        }
+        static void Print(Books[] book)
+        {
+            for (int i = 0; i < book.Length;i++)
+            {
+
+                Console.WriteLine($"{book[i].Title}, {book[i].Author}, {book[i].Year}, {book[i].IsAvailable}");
+                Console.WriteLine("============================================================");
+            }
+        }
+    }
+}
